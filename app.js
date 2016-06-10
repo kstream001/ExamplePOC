554c391b2be7aeb5ea825b51f71e82774fad6c87
Kstream 001 <kstream001@gmail.com>
2016-06-10 11:18:36 +0530

    urlchanged

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.get('/',function(req,res){
+app.get('/index',function(req,res){
 	res.sendFile(path.join(__dirname+'/index.html'));
 });
 
